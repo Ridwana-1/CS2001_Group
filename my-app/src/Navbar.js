57eaf4688ad4d79ba5f156d97c8d3c065b96e7ed
Ridwan <ridwanabdulkadir@hotmail.com>
2025-01-26 13:29:26 +0000

    new changes

---
+++ my-app/src/Navbar.js
@@ -0,0 +1,20 @@
+import React , {useState} from 'react'
+
+function Navbar() {
+  return (
+    
+    <>
+       <nav>Nav className = "Navbar"
+        <div className = "navbar-container">
+
+        </div>
+       </nav>
+       
+       
+       </>
+
+    
+  )
+}
+
+export default Navbar
