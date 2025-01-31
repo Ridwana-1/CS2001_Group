164cb04940061d56543f0d5bb5f6dde77c78bb3e
Ridwan <ridwanabdulkadir@hotmail.com>
2025-01-31 21:08:40 +0000

    deleted imports

---
+++ my-app/src/Navbar.js
@@ -0,0 +1,25 @@
+import React , {useState} from 'react'
+
+
+function Navbar() {
+  return (
+    
+    <>
+       <nav>Nav className = "Navbar"
+        <div className = "navbar-container">
+
+        </div>
+       < link to ="/"className="navbar-logo"> 
+       swap SAVIOUR
+       </link>
+
+       </nav>
+
+       
+       </>
+
+    
+  )
+}
+
+export default Navbar
